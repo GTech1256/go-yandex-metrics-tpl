b4c81da2666fff2a19429ea474982fa8208eac49
Roman Bakakin <ribakakin@avito.ru>
2023-09-30 03:27:00 +0300

    iter1 - Implement server

---
+++ internal/server/adapters/http/update/handler.go
@@ -0,0 +1,70 @@
+package update
+
+import (
+	"context"
+	"github.com/GTech1256/go-yandex-metrics-tpl/internal/domain/entity"
+	http2 "github.com/GTech1256/go-yandex-metrics-tpl/internal/server/adapters/http"
+	update_interface "github.com/GTech1256/go-yandex-metrics-tpl/internal/server/adapters/http/update/interface"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type handler struct {
+	logger        *logrus.Entry
+	updateService update_interface.Service
+}
+
+func NewHandler(logger *logrus.Entry, updateService update_interface.Service) http2.Handler {
+	return &handler{
+		logger:        logger,
+		updateService: updateService,
+	}
+}
+
+func (h handler) Register(router *http.ServeMux) {
+	router.HandleFunc("/update/", h.Update)
+}
+
+// Update /update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
+func (h handler) Update(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		h.logger.Info("Allow Only Method POST ")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	metric, err := h.updateService.GetMetric(context.Background(), request.RequestURI)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if metric.Type == entity.Gauge {
+		metricsGauge := &entity.MetricGauge{
+			Type:  metric.Type,
+			Name:  metric.Name,
+			Value: entity.GaugeValue(*metric.Value.(*float64)),
+		}
+
+		if err := h.updateService.SaveGaugeMetric(context.Background(), metricsGauge); err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if metric.Type == entity.Counter {
+		metricsCounter := &entity.MetricCounter{
+			Type:  metric.Type,
+			Name:  metric.Name,
+			Value: entity.CounterValue(*metric.Value.(*int64)),
+		}
+
+		if err := h.updateService.SaveCounterMetric(context.Background(), metricsCounter); err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	writer.Header().Add("Content-Type", "text/plain")
+	writer.WriteHeader(http.StatusOK)
+}
---
+++ internal/server/service/update.go
@@ -0,0 +1,109 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/GTech1256/go-yandex-metrics-tpl/internal/domain/entity"
+	update_interface "github.com/GTech1256/go-yandex-metrics-tpl/internal/server/adapters/http/update/interface"
+	metric2 "github.com/GTech1256/go-yandex-metrics-tpl/internal/server/domain/metric"
+	"github.com/sirupsen/logrus"
+	"strconv"
+	"strings"
+)
+
+var (
+	// ErrNotCorrectUrl возвращает ошибку, если формат url не подошел
+	ErrNotCorrectUrl   = errors.New("Not Correct Url")
+	ErrNotCorrectType  = errors.New("Not Correct metricType")
+	ErrNotCorrectValue = errors.New("Not Correct metricValue")
+)
+
+type updateService struct {
+	logger  *logrus.Entry
+	storage metric2.Storage
+}
+
+func NewUpdateService(logger *logrus.Entry, storage metric2.Storage) update_interface.Service {
+	return &updateService{logger: logger, storage: storage}
+}
+
+func (u updateService) GetMetric(ctx context.Context, url string) (*entity.Metric, error) {
+	const count = 5
+	splitted := strings.Split(url, "/")
+	u.logger.Info(splitted)
+
+	if len(splitted) != count {
+		u.logger.Info()
+		u.logger.Info("len(splitted) != count", len(splitted), splitted)
+		fmt.Printf("%+v", splitted)
+
+		return nil, ErrNotCorrectUrl
+	}
+
+	metricTypUnsafe, metricName, metricValueUnsafe := splitted[2], splitted[3], splitted[4]
+
+	metricType := getValidType(metricTypUnsafe)
+	if metricType == entity.NoType {
+		return nil, ErrNotCorrectType
+	}
+
+	var metricValue interface{}
+	var err error
+	if metricType == entity.Gauge {
+		metricValue, err = getTypeGaugeValue(metricValueUnsafe)
+		if err != nil {
+			return nil, ErrNotCorrectValue
+		}
+
+	} else if metricType == entity.Counter {
+		metricValue, err = getTypeCounterValue(metricValueUnsafe)
+		if err != nil {
+			return nil, ErrNotCorrectValue
+		}
+	} else {
+		return nil, ErrNotCorrectValue
+	}
+
+	return &entity.Metric{
+		Type:  metricType,
+		Name:  metricName,
+		Value: metricValue,
+	}, nil
+}
+
+func getValidType(metricType string) entity.Type {
+	switch entity.Type(metricType) {
+	case entity.Gauge:
+		return entity.Gauge
+	case entity.Counter:
+		return entity.Counter
+	default:
+		return entity.NoType
+	}
+}
+
+func getTypeGaugeValue(metricValueUnsafe string) (*float64, error) {
+	metricValue, err := strconv.ParseFloat(metricValueUnsafe, 64)
+	if err != nil {
+		return nil, ErrNotCorrectValue
+	}
+
+	return &metricValue, nil
+}
+
+func getTypeCounterValue(metricValueUnsafe string) (*int64, error) {
+	metricValue, err := strconv.ParseInt(metricValueUnsafe, 10, 64)
+	if err != nil {
+		return nil, ErrNotCorrectValue
+	}
+
+	return &metricValue, nil
+}
+
+func (u updateService) SaveGaugeMetric(ctx context.Context, metric *entity.MetricGauge) error {
+	return u.storage.SaveGauge(ctx, metric)
+}
+func (u updateService) SaveCounterMetric(ctx context.Context, metric *entity.MetricCounter) error {
+	return u.storage.SaveCounter(ctx, metric)
+}
